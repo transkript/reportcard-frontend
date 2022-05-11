1ab92d07aa466a967cd51278fb17c944a52d7440
Elroy Kanye <elroykanye@gmail.com>
2022-05-11 18:38:57 +0100

    add components to app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./modules/rc-dashboard/components/dashboard/dashboard.component";
+import {RcHomeComponent} from "./modules/rc-dashboard/components/rc-home/rc-home.component";
+import {RcSubjectsComponent} from "./modules/rc-dashboard/components/rc-subjects/rc-subjects.component";
+import {RcStudentsComponent} from "./modules/rc-dashboard/components/rc-students/rc-students.component";
+import {RcClassesComponent} from "./modules/rc-dashboard/components/rc-classes/rc-classes.component";
+import {RcClasslistsComponent} from "./modules/rc-dashboard/components/rc-classlists/rc-classlists.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: DashboardComponent,
+    path: 'dashboard',
+    children: [
+      {component: RcHomeComponent, path: 'home'},
+      {component: RcSubjectsComponent, path: 'subjects'},
+      {component: RcStudentsComponent, path: 'students'},
+      {component: RcClassesComponent, path: 'classes'},
+      {component: RcClasslistsComponent, path: 'class-lists'},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SampleComponent } from './components/sample/sample.component';
+import {RcDashboardModule} from "./modules/rc-dashboard/rc-dashboard.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SampleComponent
+    SampleComponent,
   ],
   imports: [
+    RcDashboardModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
