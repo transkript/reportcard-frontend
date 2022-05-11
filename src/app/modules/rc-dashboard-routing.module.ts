336e8eb7de2325c83cd1bdbebdcb41725eeb1f66
Elroy Kanye <elroykanye@gmail.com>
2022-05-11 18:37:08 +0100

    add routes to dashboard

---
+++ src/app/modules/rc-dashboard-routing.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {RcHomeComponent} from "./rc-dashboard/components/rc-home/rc-home.component";
+
+const routes: Routes = [
+  {component: RcHomeComponent, path: 'home'}
+]
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class RcDashboardRoutingModule {
+}
