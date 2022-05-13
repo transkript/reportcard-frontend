07cc9b39de1d174cf4350457a6478752bda6a5bf
Elroy Kanye <elroykanye@gmail.com>
2022-05-13 15:44:16 +0100

    add homeStats and stats value display

--- src/app/modules/rc-dashboard/components/rc-home/rc-home.component.ts
+++ src/app/modules/rc-dashboard/components/rc-home/rc-home.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rc-home.component.scss']
 })
 export class RcHomeComponent implements OnInit {
+  title: string = 'Home';
+  homeStats: {title: string; icon: string; value: number; link: string}[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.homeStats = [
+      {title: 'Students', icon: 'pi pi-user', value: 24, link: '#'},
+      {title: 'Subjects', icon: 'pi pi-user', value: 32, link: '#'},
+      {title: 'Classes', icon: 'pi pi-user', value: 14, link: '#'},
+      {title: 'Sections', icon: 'pi pi-user', value: 4, link: '#'},
+    ]
+  }
+
+  homeStatsValueDisplay(value: number): string {
+    let valueDisplay: string = value.toString();
+    if(valueDisplay.length > 3) {
+      valueDisplay = '999+'
+    } else {
+      const zeros = 3 - valueDisplay.length;
+      for(let i = 0; i < zeros; i++) {
+        valueDisplay = '0' + valueDisplay;
+      }
+    }
+    return valueDisplay;
   }
 
 }
