fe5aec0862918701c571c01b6966b08abbe0c6bc
Elroy Kanye <elroykanye@gmail.com>
2022-05-13 15:43:03 +0100

    adjust content wrap for dashbody

--- src/app/modules/rc-dashboard/components/dashbody/dashbody.component.ts
+++ src/app/modules/rc-dashboard/components/dashbody/dashbody.component.ts
@@ -5,9 +5,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashbody.component.scss'],
   template: `
     <div class="dash-body">
-      <span class="h2 text-primary text-capitalize">Title</span>
+      <div class="container-fluid dash-body-content">
+        <div class="title-container py-2">
+          <span class="h2 text-primary font-weight-bold text-capitalize">Title</span>
+        </div>
 
-      <router-outlet ></router-outlet>
+        <div class="py-3 mx-1">
+          <router-outlet ></router-outlet>
+        </div>
+      </div>
     </div>
   `
 })
