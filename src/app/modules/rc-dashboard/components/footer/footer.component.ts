47c2b4b908202c87d64e45f17c5a94eca29ae774
Elroy Kanye <elroykanye@gmail.com>
2022-05-13 15:41:16 +0100

    added content to footer

--- src/app/modules/rc-dashboard/components/footer/footer.component.ts
+++ src/app/modules/rc-dashboard/components/footer/footer.component.ts
@@ -4,10 +4,22 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <p>footer works!</p>
+    <div class="d-flex">
+      <div class="">
+        Copyright &copy; {{ currentYear }}
+      </div>
+      <span class="spacer"></span>
+      <div class="">
+        <button pButton pRipple type="button" icon="pi pi-facebook" class="p-button-rounded"></button>&nbsp;
+        <button pButton pRipple type="button" icon="pi pi-twitter" class="p-button-rounded"></button>&nbsp;
+        <button pButton pRipple type="button" icon="pi pi-instagram" class="p-button-rounded"></button>&nbsp;
+        <button pButton pRipple type="button" icon="pi pi-github" class="p-button-rounded"></button>&nbsp;
+      </div>
+    </div>
   `
 })
 export class FooterComponent implements OnInit {
+  currentYear: number = new Date().getFullYear();
 
   constructor() { }
 
