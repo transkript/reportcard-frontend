2b7a5b6e77f0087dff7d2ad61a29c26236601980
godsakani <nyonggodwill11@gmail.com>
2022-05-19 13:14:09 +0100

    feat: added auth module to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SampleComponent } from './components/sample/sample.component';
+import { RcAuthModule } from './modules/rc-auth/rc-auth.module';
 import {RcDashboardModule} from "./modules/rc-dashboard/rc-dashboard.module";
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SampleComponent,
+   
   ],
   imports: [
     RcDashboardModule,
+    RcAuthModule,
     BrowserModule,
     AppRoutingModule,
   ],
