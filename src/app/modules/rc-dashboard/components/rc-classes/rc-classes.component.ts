ed853912bcf85d89ac521ad493ad34acffedfda1
Elroy Kanye <elroykdev@gmail.com>
2022-05-24 05:01:55 +0100

    add class level related impl

--- src/app/modules/rc-dashboard/components/rc-classes/rc-classes.component.ts
+++ src/app/modules/rc-dashboard/components/rc-classes/rc-classes.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {ClassLevel} from "../../../../models/dto/classlevel.model";
+import {ClassLevelService} from "../../../../services/class-level.service";
+import {MessageService} from "primeng/api";
+import {addToMessageService} from "../../../../utils/message-service.util";
 
 @Component({
   selector: 'app-rc-classes',
@@ -7,10 +11,27 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RcClassesComponent implements OnInit {
 
-  constructor() {
+  classLevels: ClassLevel[] = [];
+
+  constructor(private classLevelService: ClassLevelService, private messageService: MessageService) {
   }
 
   ngOnInit(): void {
+    this.loadClasses();
   }
 
+  saveClassAction() {
+
+  }
+
+  loadClasses() {
+    this.classLevelService.getClassLevels().subscribe({
+      next: (classLevels: ClassLevel[]) => {
+        this.classLevels = classLevels;
+      },
+      error: (error) => {
+        addToMessageService('error', 'Error loading classes', `Server not responding.\n${error.message}`, this.messageService);
+      }
+    });
+  }
 }
