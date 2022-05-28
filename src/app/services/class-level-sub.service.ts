fe65d497b7278be647afc9b3f80d6662cb4ec6a2
Elroy Kanye <elroykdev@gmail.com>
2022-05-28 20:22:48 +0100

    add put api client for level sub

--- src/app/services/class-level-sub.service.ts
+++ src/app/services/class-level-sub.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {RC_CLASS_LEVEL_SUB_API_URL} from "../app.constants";
 import {Observable} from "rxjs";
 import {ClassLevelSub} from "../models/dto/classlevelsub.model";
+import {EntityResponse} from "../models/dto/entity.response";
 
 @Injectable({
   providedIn: 'root'
@@ -20,4 +21,7 @@ export class ClassLevelSubService {
     return this.http.get<ClassLevelSub[]>(this.classLevelSubApiUrl + "/level/" + classLevelId);
   }
 
+  updateClassLevelSub(classLevelSub: ClassLevelSub): Observable<EntityResponse> {
+    return this.http.put(`${this.classLevelSubApiUrl}/${classLevelSub.id}`, classLevelSub);
+  }
 }
