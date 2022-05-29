effd5769b34d9fcb8fc0389101eb8e65d3404175
Elroy Kanye <elroykdev@gmail.com>
2022-05-29 23:27:18 +0100

    implement client sequence observer

--- src/app/services/sequence.service.ts
+++ src/app/services/sequence.service.ts
@@ -1,10 +1,19 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
+import {RC_SEQUENCE_API_URL} from "../app.constants";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Sequence} from "../models/dto/sequence.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SequenceService {
 
-  constructor() {
+  constructor(@Inject(RC_SEQUENCE_API_URL) private apiUrl: string, private http: HttpClient) {
   }
+
+  getAllSequences(): Observable<Sequence[]> {
+    return this.http.get<Sequence[]>(this.apiUrl);
+  }
+
 }
