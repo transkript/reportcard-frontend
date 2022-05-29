72b486fb704ae045b360c29211f08375902fac40
Elroy Kanye <elroykdev@gmail.com>
2022-05-29 23:33:40 +0100

    add class list and sub regs api urls

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -7,6 +7,8 @@ export const RC_CLASS_LEVEL_API_URL = `${RC_SERVER_URL}/api/class_level`;
 
 export const RC_CLASS_LEVEL_SUB_API_URL = `${RC_SERVER_URL}/api/class_level_sub`;
 
+export const RC_CLASS_LIST_API_URL = `${RC_SERVER_URL}/api/class_list`;
+
 export const RC_GRADE_API_URL = `${RC_SERVER_URL}/api/grade`;
 
 export const RC_SCHOOL_API_URL = `${RC_SERVER_URL}/api/school`;
@@ -20,5 +22,7 @@ export const RC_STUDENT_API_URL = `${RC_SERVER_URL}/api/student`;
 export const RC_STUDENT_APPLICATION_API_URL = `${RC_SERVER_URL}/api/student-application`;
 
 export const RC_SUBJECT_API_URL = `${RC_SERVER_URL}/api/subject`;
+
+export const RC_SUBJECT_REGISTRATION_API_URL = `${RC_SERVER_URL}/api/subject_registration`;
 
 export const RC_TERM_API_URL = `${RC_SERVER_URL}/api/term`;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import {RcAuthModule} from './modules/rc-auth/rc-auth.module';
 import {RcDashboardModule} from "./modules/rc-dashboard/rc-dashboard.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {HttpClientModule} from "@angular/common/http";
-import {injectables} from "./services/injectables/rc-api.injectables";
+import {injectables} from "./app.injectables";
 import {MessageService} from "primeng/api";
 import {NgbActiveModal, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
