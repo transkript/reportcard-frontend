fba6f40b594f93c47fcb3dae4b8ddb6d4ebd0bae
Elroy Kanye <elroykdev@gmail.com>
2022-05-29 23:34:10 +0100

    impl get all for client academic year service

--- src/app/services/academic-year.service.ts
+++ src/app/services/academic-year.service.ts
@@ -1,10 +1,19 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {AcademicYear} from "../models/dto/academicyear.model";
+import {RC_ACADEMIC_YEAR_API_URL} from "../app.constants";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AcademicYearService {
 
-  constructor() {
+  constructor(@Inject(RC_ACADEMIC_YEAR_API_URL) private apiUrl: string, private http: HttpClient) {
+
+  }
+
+  getAllAcademicYears(): Observable<AcademicYear[]> {
+    return this.http.get<AcademicYear[]>(this.apiUrl);
   }
 }
