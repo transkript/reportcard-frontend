968fdbde87acf784994614d359f95623f07fb8ea
Elroy Kanye <elroykdev@gmail.com>
2022-06-09 14:03:01 +0100

    refactor crud methods for subjects

--- src/app/modules/rc-dashboard/components/rc-subjects/rc-subjects.component.ts
+++ src/app/modules/rc-dashboard/components/rc-subjects/rc-subjects.component.ts
@@ -36,19 +36,33 @@ export class RcSubjectsComponent implements OnInit {
     })
   }
 
-  deleteSubject(subject: Subject) {
+  saveSubjectAction(subject?: Subject) {
+    const modalRef = this.modalService.open(SaveSubjectComponent, {
+      size: 'lg', centered: true, backdrop: 'static', keyboard: true
+    });
+    const saveSubjectComponent: SaveSubjectComponent = modalRef.componentInstance;
+    if(!subject) {
+      saveSubjectComponent.resetSubject();
+      saveSubjectComponent.subjectForm.reset();
+    } else {
+      saveSubjectComponent.subject = subject;
+      saveSubjectComponent.setupSubjectForm();
+    }
+    modalRef.result.then(() => {
+      this.loadSubjects();
+    });
+  }
+
+  deleteSubjectAction(subject: Subject) {
     const confirmDelete: boolean = confirm("Are you sure want to delete " + subject.name);
     if (confirmDelete) {
       this.subjectService.deleteSubject(subject.id).subscribe({
+        // TODO remove this logs
         next: (res) => {
-          console.log(res);
+          addToMessageService(this.msgService, 'error', 'Error', res.message);
+          this.loadSubjects();
         },
-        error: (err) => {
-          console.log(err);
-          this.messageService.add({
-            severity: 'error', summary: 'Error', detail: err.message
-          })
-        },
+        error: (err) => addToMessageService(this.msgService, 'error', 'Error', err.message)
       });
     }
 
