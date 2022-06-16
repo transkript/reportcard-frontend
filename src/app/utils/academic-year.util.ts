7fdbc876d2c06902d4c28ea257ee44c402d5851a
Elroy Kanye <elroykdev@gmail.com>
2022-06-16 23:47:18 +0100

    add module for acad year utils

---
+++ src/app/utils/academic-year.util.ts
@@ -0,0 +1,22 @@
+export module AcademicYearUtil {
+  export const isValid = (yearName: string): boolean => {
+    const yearParts = yearName.split("/");
+    if(yearParts.length == 2) {
+      const ys = [];
+      for (let i = 0; i < yearParts.length; i++) {
+        const part = yearParts[i].trim();
+        if(part.length != 4) return false;
+        else {
+          try {
+            ys.push(parseInt(part));
+          } catch (e) {
+            return false;
+          }
+        }
+      }
+      const [y1, y2]: number[] = [...ys];
+      return y1 == y2 - 1;
+    }
+    return false;
+  }
+}
