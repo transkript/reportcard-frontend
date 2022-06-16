ba381feb9e73f4a0d625f4dc5b42bcbc4f5ee558
Elroy Kanye <elroykdev@gmail.com>
2022-06-16 23:46:08 +0100

    imple add and update client services for acade year

--- src/app/services/academic-year.service.ts
+++ src/app/services/academic-year.service.ts
@@ -3,6 +3,7 @@ import {Observable} from "rxjs";
 import {AcademicYear} from "../models/dto/academicyear.model";
 import {RC_ACADEMIC_YEAR_API_URL} from "../app.constants";
 import {HttpClient} from "@angular/common/http";
+import {EntityResponse} from "../models/dto/entity.response";
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,13 @@ export class AcademicYearService {
 
   getAllAcademicYears(): Observable<AcademicYear[]> {
     return this.http.get<AcademicYear[]>(this.apiUrl);
+  }
+
+  addAcademicYear(academicYear: AcademicYear): Observable<EntityResponse> {
+    return this.http.post<EntityResponse>(`${this.apiUrl}`, academicYear);
+  }
+
+  updateAcademicYear(academicYear: AcademicYear): Observable<EntityResponse> {
+    return this.http.put<EntityResponse>(`${this.apiUrl}/${academicYear.id}`, academicYear);
   }
 }
