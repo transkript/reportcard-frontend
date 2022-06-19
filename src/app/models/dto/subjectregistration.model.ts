121c26cda2f94604b9924da2cc04df1e6d80ec0a
Elroy Kanye <elroykdev@gmail.com>
2022-06-19 21:45:59 +0100

    update service and model for subreg

--- src/app/models/dto/subjectregistration.model.ts
+++ src/app/models/dto/subjectregistration.model.ts
@@ -1,9 +1,8 @@
 export interface SubjectRegistration {
   id: number;
-  created_at: string;
-  updated_at: string;
-  applicationId: number;
   student_id: number;
   year_id: number;
-  subjectId: number;
+  subject_id: number;
+  created_at?: string;
+  updated_at?: string;
 }
--- src/app/services/subject-registration.service.ts
+++ src/app/services/subject-registration.service.ts
@@ -3,6 +3,7 @@ import {RC_SUBJECT_REGISTRATION_API_URL} from "../app.constants";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {SubjectRegistration} from "../models/dto/subjectregistration.model";
+import {EntityResponse} from "../models/dto/entity.response";
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,15 @@ export class SubjectRegistrationService {
   constructor(@Inject(RC_SUBJECT_REGISTRATION_API_URL) private apiUrl: string, private http: HttpClient) {
   }
 
-  getSubjectRegistration(id: number): Observable<SubjectRegistration> {
+  getOne(id: number): Observable<SubjectRegistration> {
     return this.http.get<SubjectRegistration>(`${this.apiUrl}/${id}`);
+  }
+
+  add(subjectRegistration: SubjectRegistration): Observable<EntityResponse> {
+    return this.http.post<SubjectRegistration>(`${this.apiUrl}`, subjectRegistration);
+  }
+
+  addMultiple(subjectRegistrations: SubjectRegistration[]): Observable<EntityResponse> {
+    return this.http.post<SubjectRegistration>(`${this.apiUrl}/multiple`, subjectRegistrations);
   }
 }
